64d300769587f9968191eb898d767ba076b3e5fd
Alex Kiborgok <borgokalek14@gmail.com>
2022-03-12 21:43:30 +0300

    implement toggle effect on what we do section

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".design").click(function () {
+    $(".design-description").toggle()
+    $(".design-icon").toggle();
+})
+
+$(".develop").click(function () {
+  $(".develop-description").toggle();
+  $(".develop-icon").toggle();
+});
+
+$(".product").click(function () {
+  $(".product-description").toggle();
+  $(".product-icon").toggle();
+});
