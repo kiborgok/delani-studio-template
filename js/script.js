0b67acc3c5ae4f43ff9e2a0d23df51d76a514e17
Alex Kiborgok <borgokalek14@gmail.com>
2022-03-13 18:24:19 +0300

    add form submission function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,52 @@
-$(".product").click(function () {
-    $(this).find(".description").toggle()
-    $(this).find(".icon").toggle();
-})
+$(document).ready(function () {
+  //Show form popup after 1 second on page load
 
-$(".port-imgs").hover(function () {
-    $(this).find(".port-overlay").toggle()
-})
+  setTimeout(function () {
+    $(".popup").show("slow");
+  }, 1000);
+
+  //Show and hide icon and description on click
+
+  $(".product").click(function () {
+    $(this).find(".description").toggle();
+    $(this).find(".icon").toggle();
+  });
+
+  //Show project name on hover
+
+  $(".port-imgs").hover(function () {
+    $(this).find(".port-overlay").toggle();
+  });
+
+  //Hide form when 'X' is clicked
+
+  $(".text-end").click(function () {
+    setTimeout(function () {
+      $(".popup").hide("slow");
+    }, 1000);
+  });
+
+  //Show thank you message or error message when submit button is clicked
+
+  $(".show-msg").click(function () {
+    $("form").submit(function (e) {
+      e.preventDefault();
+      let name = $(this).find("#name").val();
+      let email = $(this).find("#email").val();
+      let msg = $(this).find("#textarea").val();
+      if (name == "" || email == "" || msg == "") {
+          $(this).find(".ms").text("Please fill all fields");
+          return
+      } else {
+        $(this).find(".ms").text(
+          name +
+            " we have received your message. Thank you for reaching out to us."
+        );
+          setTimeout(function () {
+            $(".ms").remove();
+          $(".popup").hide("slow");
+        }, 6000);
+      }
+    });
+  });
+});
